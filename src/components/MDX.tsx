6f412aa71f89ad8d6e9134906e64a09b9eb6e57e
gxz <cnmmail163@163.com>
2024-09-09 12:19:28 +0800

    anchor

--- src/components/MDX.tsx
+++ src/components/MDX.tsx
@@ -5,11 +5,14 @@ import RefTopic from "@/components/RefTopic";
 import RefComment from "@/components/RefComment";
 import {memo} from "react";
 import "github-markdown-css";
+import Anchor from "@/components/Anchor";
 
 function MDX({children}: { children: string }) {
     return (
         <Await fn={() => serialize(children)}>
-            {res => <article className="markdown-body" style={{background: 'transparent'}}><MDXRemote {...res} components={{RefTopic, RefComment}}/></article>}
+            {res => <article className="markdown-body" style={{background: 'transparent'}}>
+                <MDXRemote{...res} components={{RefTopic, RefComment, a: (props: any) => <Anchor {...props}/>}}/>
+            </article>}
         </Await>
     )
 }
