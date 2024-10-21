5fd19353f68f6bed42856fcca9d04eecb4c09f52
gxz <cnmmail163@163.com>
2024-10-21 12:17:47 +0800

    自动审核

--- src/prisma/post.ts
+++ src/prisma/post.ts
@@ -1,7 +1,17 @@
 import prisma from "@/prisma/index";
+import {importWrapKey} from "@/crypto/asymmetric";
 
-export default (data: {
+const sensitive = fetch('https://raw.githubusercontent.com/cjh0613/tencent-sensitive-words/refs/heads/main/sensitive_words_lines.txt')
+    .then(res => res.text())
+    .then(value => new RegExp(value.split('\n').join('|')))
+
+export default async (data: {
     keyVerify: Buffer
     keyWrap: Buffer
     message: string
-}) => prisma.topic.create({data, select: {id: true}}).then(({id}) => id)
+}) => {
+    await importWrapKey(data.keyWrap)
+    const match = data.message.match(await sensitive)
+    if (match) throw new Error(`触发敏感词“${match.join('”、“')}”`)
+    return prisma.topic.create({data, select: {id: true}}).then(({id}) => id)
+}
