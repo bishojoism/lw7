a12339447a5898835986faaf201aafad599ef142
bishojoism <146826524+bishojoism@users.noreply.github.com>
2025-03-18 14:45:28 +0800

    审核

--- src/server/POST.ts
+++ src/server/POST.ts
@@ -5,10 +5,18 @@ import from from "@/base64/from";
 import auSchema from "@/server/auSchema";
 import post from "@/prisma/post";
 
+const sensitive = fetch('https://raw.gitmirror.com/konsheng/Sensitive-lexicon/refs/heads/main/Vocabulary/零时-Tencent.txt')
+    .then(res => res.text())
+    .then(value => {
+        const words = value.split('\n')
+        words.pop()
+        return new RegExp(words.map(string => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|'), 'g')
+    })
+
 const schema = z.object({
     keyVerify: z.string().transform(from),
     keyWrap: z.string().transform(from),
-    message: z.string()
+    message: z.string().transform(arg => arg.replaceAll(await sensitive, match => '\\*'.repeat(match.length)))
 }).strict()
 
 export default server(async request => {
